7387f60b233baa25d1b193c276fe8bea1f2573ef
hjkang306 <109262091+fkdldhs8484@users.noreply.github.com>
2022-09-28 20:33:30 +0900

    08/33
    a

--- javascript/assets/js/common.js
+++ javascript/assets/js/common.js
@@ -1,35 +1,34 @@
-hljs.highlightAll();
+hljs.highlightAll()
 
-        // 모달
-        const modalBtn = document.querySelector(".modal__btn");
-        const modalClose = document.querySelector(".modal__close");
-        const modalCont = document.querySelector(".modal__cont");
+// 모달
+const modalBtn = document.querySelector('.modal__btn')
+const modalClose = document.querySelector('.modal__close')
+const modalCont = document.querySelector('.modal__cont')
 
-        modalBtn.addEventListener("click", () => {
-            modalCont.classList.add("show");
-            modalCont.classList.remove("hide");
-        });
-        modalClose.addEventListener("click", () => {
-            modalCont.classList.add("hide");
-        });
+modalBtn.addEventListener('click', () => {
+  modalCont.classList.add('show')
+  modalCont.classList.remove('hide')
+})
+modalClose.addEventListener('click', () => {
+  modalCont.classList.add('hide')
+})
 
-        // 탭 메뉴
-        const tabBtn = document.querySelectorAll(".modal__box .tabs > div");
-        const tabCont = document.querySelectorAll(".modal__box .cont > div");
+// 탭 메뉴
+const tabBtn = document.querySelectorAll('.modal__box .tabs > div')
+const tabCont = document.querySelectorAll('.modal__box .cont > div')
 
-        tabBtn.forEach((element, index) => {
-            element.addEventListener("click", (event) => {
+tabBtn.forEach((element, index) => {
+  element.addEventListener('click', (event) => {
+    event.preventDefault()
 
-                event.preventDefault();
+    tabBtn.forEach((el) => {
+      el.classList.remove('active')
+    })
+    element.classList.add('active')
 
-                tabBtn.forEach(el => {
-                    el.classList.remove("active");
-                })
-                element.classList.add("active");
-
-                tabCont.forEach(div => {
-                    div.style.display = "none";
-                })
-                tabCont[index].style.display = "block";
-            });
-        });
+    tabCont.forEach((div) => {
+      div.style.display = 'none'
+    })
+    tabCont[index].style.display = 'block'
+  })
+})
