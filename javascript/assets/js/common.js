e612555a8cf4f016abddd1e21c73a7c63363f389
이유나 <109262091+fkdldhs8484@users.noreply.github.com>
2022-10-20 00:18:57 +0900

    10/20
    a

--- javascript/assets/js/common.js
+++ javascript/assets/js/common.js
@@ -1,34 +1,36 @@
-hljs.highlightAll()
+hljs.highlightAll();
 
 // 모달
-const modalBtn = document.querySelector('.modal__btn')
-const modalClose = document.querySelector('.modal__close')
-const modalCont = document.querySelector('.modal__cont')
+const modalBtn = document.querySelector(".modal__btn");
+const modalClose = document.querySelector(".modal__close");
+const modalCont = document.querySelector(".modal__cont");
+const iframeCont = document.querySelector(".modal__cont .cont .iframe");
 
-modalBtn.addEventListener('click', () => {
-  modalCont.classList.add('show')
-  modalCont.classList.remove('hide')
-})
-modalClose.addEventListener('click', () => {
-  modalCont.classList.add('hide')
-})
+modalBtn.addEventListener("click", () => {
+    modalCont.classList.add("show");
+    modalCont.classList.remove("hide");
+    iframeCont.classList.remove("blind");
+});
+modalClose.addEventListener("click", () => {
+    modalCont.classList.add("hide");
+    iframeCont.classList.add("blind");
+});
 
 // 탭 메뉴
-const tabBtn = document.querySelectorAll('.modal__box .tabs > div')
-const tabCont = document.querySelectorAll('.modal__box .cont > div')
+const tabBtn = document.querySelectorAll(".modal__box .tabs > div");
+const tabCont = document.querySelectorAll(".modal__box .cont > div");
 
-tabBtn.forEach((element, index) => {
-  element.addEventListener('click', (event) => {
-    event.preventDefault()
+tabBtn.forEach((e, i) => {
+    e.addEventListener("click", (event) => {
+        event.preventDefault(); // 버튼을 클릭해도 스크롤 제자리
 
-    tabBtn.forEach((el) => {
-      el.classList.remove('active')
-    })
-    element.classList.add('active')
-
-    tabCont.forEach((div) => {
-      div.style.display = 'none'
-    })
-    tabCont[index].style.display = 'block'
-  })
-})
+        tabBtn.forEach((e) => {
+            e.classList.remove("active");
+        });
+        e.classList.add("active");
+        tabCont.forEach((div) => {
+            div.style.display = "none";
+        });
+        tabCont[i].style.display = "block";
+    });
+});
